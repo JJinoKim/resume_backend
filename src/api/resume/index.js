791b2b774249289bfa9319fc96ef1def7831935b
JJinoKim <shawar1@naver.com>
2021-12-28 14:43:47 +0900

    ss

--- src/api/resume/index.js
+++ src/api/resume/index.js
@@ -1,9 +1,11 @@
 import Router from 'koa-router';
 import * as resumeCtrl from './resume.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 
 const resume = new Router();
 
 resume.get('/',resumeCtrl.list);
+resume.post('/write',checkLoggedIn,resumeCtrl.write);
 
 resume.use('/:id', resumeCtrl.getResumeById, resume.routes());
 
